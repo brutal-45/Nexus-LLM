fb4d3df99caff82972847e5398114009df99afac
Viraj Jadhav <creatorsports81@gmail.com>
2026-08-26 15:34:52 +0530

    Update __init__.py

--- nexus_llm/distillation/__init__.py
+++ nexus_llm/distillation/__init__.py
@@ -5,8 +5,8 @@ a larger teacher model using soft-label distillation with configurable
 temperature and loss balancing.
 """
 
-from nexus_llm.distillation.distiller import Distiller
 from nexus_llm.distillation.config import DistillationConfig
+from nexus_llm.distillation.distiller import Distiller
 from nexus_llm.distillation.trainer import DistillationTrainer
 
-__all__ = ["Distiller", "DistillationConfig", "DistillationTrainer"]
+__all__ = ["DistillationConfig", "DistillationTrainer", "Distiller"]
