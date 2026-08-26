fe8fbcb337f8285cce0ac375ccf477c6d99e61d3
Viraj Jadhav <creatorsports81@gmail.com>
2026-08-26 15:31:56 +0530

    Update __init__.py

--- nexus_llm/core/__init__.py
+++ nexus_llm/core/__init__.py
@@ -1,13 +1,24 @@
 """Core module for Nexus-LLM."""
+
 from nexus_llm.core.config import Settings, get_settings
-from nexus_llm.core.model_catalog import MODEL_CATALOG, get_model_info, list_models
 from nexus_llm.core.exceptions import (
-    ModelNotFoundError, ModelLoadError, InferenceError,
-    ConfigurationError, TrainingError
+    ConfigurationError,
+    InferenceError,
+    ModelLoadError,
+    ModelNotFoundError,
+    TrainingError,
 )
+from nexus_llm.core.model_catalog import MODEL_CATALOG, get_model_info, list_models
 
 __all__ = [
-    "Settings", "get_settings", "MODEL_CATALOG", "get_model_info", "list_models",
-    "ModelNotFoundError", "ModelLoadError", "InferenceError",
-    "ConfigurationError", "TrainingError",
+    "MODEL_CATALOG",
+    "ConfigurationError",
+    "InferenceError",
+    "ModelLoadError",
+    "ModelNotFoundError",
+    "Settings",
+    "TrainingError",
+    "get_model_info",
+    "get_settings",
+    "list_models",
 ]
